db144732a504eb5c67839ffb7b7c575902048da3
yashsharma9311 <yashppsharma26@gmail.com>
2025-06-10 18:41:00 +0530

    change

---
+++ dom1.js
@@ -0,0 +1,13 @@
+// let see=()=>{
+//     let text = document.querySelector("#text");
+//     let see = document.querySelector("#Btn").value 
+//     text.innerHTML= see;
+// }
+
+let see=()=>{
+    let text = document.querySelector("#text");
+    let see = document.querySelector("#Btn1").value 
+    let see1 = document.querySelector("#Btn2").value 
+    text.innerHTML= Number(see) + Number(see1)
+}
+
